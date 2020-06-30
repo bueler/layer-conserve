4809a80892f812bf9c82456dab854ba7a8cfe8e4
Ed Bueler <elbueler@alaska.edu>
2020-06-29 17:15:57 -0800

    fiddle around checking the p-laplacian inequalities

--- paper/pineq/inequalitysurf.m
+++ paper/pineq/inequalitysurf.m
@@ -8,15 +8,26 @@ ss = -1:.01:.99;
 [ttt,sss]=meshgrid(tt,ss);
 
 figure(1)
-surf(sss,ttt,f(ttt,sss,p)), colorbar, shading('interp')
+zzz = f(ttt,sss,p);
+surf(sss,ttt,zzz), colorbar, shading('interp')
+xlabel s, ylabel t
 title('f(t,s)')
+fprintf('f(t,s) bounded below by %.6f, versus 2^(2-p) = %.6f\n',min(min(zzz)),2^(2-p))
 
 figure(2)
+t = 0:.01:.99;
+plot(t,(1-t.^(p-1))./(1-t).^(p-1))
+xlabel t
+axis([0 1 0 4])
+
+return   % comment out to see more figures
+
+figure(3)
 g = @(t,s,p) s .* (2-p) .* t .* (t.^(p-2) + 1) + ((p-1) * (t.^p+1) - (t.^(p-2) + t.^2));
 surf(sss,ttt,g(ttt,sss,p)), colorbar, shading('interp')
 title('g(t,s) = df/ds / (positive factor)')
 
-figure(3)
+figure(4)
 plot(tt,(p-1)*(tt.^p+1),'r',tt,(p-2)*(tt.^(p-1)+tt)+tt.^(p-2)+tt.^2,'b')
 legend('should be above','should be below','location','southeast')
 
--- paper/pineq/symbolic.py
+++ paper/pineq/symbolic.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from sympy import *
-
-init_printing(use_unicode=True)
+#init_printing(use_unicode=True)
 
 s, t, p = symbols('s t p')
 
@@ -13,12 +12,13 @@ dfds = diff(f,s)
 y = dfds / (t * (1 - 2*s*t + t**2)**(-1-p/2))
 
 g = powsimp( collect(simplify(y), s) )
+print(g)
+gshouldbe = s*(2-p)*t*(t**(p-2)+1) + (p-1)*(t**p+1) - t**(p-2) - t**2
+print(simplify(g - gshouldbe))  # get 0
 
-print g
-#print collect(z.coeff(s), t)
-#print collect(simplify(z - s*z.coeff(s)), t)
+mydgds = (2-p)*t*(t**(p-2)+1)
+print(simplify(diff(g,s) - mydgds))
 
 G = g.subs(s,1)
+print(G)
 
-print G
-print factor(simplify(G + t**(p-2) + t**2 - (p-1)*(t**p+1)))
